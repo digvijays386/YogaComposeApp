6c443a447f844df0a3cafac5db8784d4d56f0ace
Digvijay Singh <digvijay.singh1@embibe.com>
2023-12-05 22:39:07 +0000

    fix coloue scheme issue

--- app/src/main/java/com/digvijay/yogaadminapp/ui/theme/Theme.kt
+++ app/src/main/java/com/digvijay/yogaadminapp/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 package com.digvijay.yogaadminapp.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
@@ -9,7 +8,6 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -23,23 +21,11 @@ private val LightColorScheme = lightColors(
     primary = Purple40,
     primaryVariant = PurpleGrey40,
     secondary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun YogaAdminAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -57,7 +43,7 @@ fun YogaAdminAppTheme(
     }
 
     MaterialTheme(
-        colors = colorScheme,
+        colors = LightColorScheme,
         typography = Typography,
         content = content
     )
